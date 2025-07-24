b7c914275307ea78a01899f202b948f22b04f976
John Goughenour <goug76@gmail.com>
2025-07-24 17:37:13 -0400

    Added social sharing

--- public/script.js
+++ public/script.js
@@ -171,6 +171,18 @@ function fetchAndDisplayTerm(term) {
           <p>${data.related.map(t => `<button class="related-btn" data-term="${t}">${t}</button>`).join(" ")}</p>
         </div>` : ""}
     </div>
+    <div id="shareButtons" class="share-buttons">
+      <p><strong>📣 Share this explanation:</strong></p>
+      <a id="twitterShare" href="#" target="_blank" title="Share on X/Twitter">
+        <img src="img/x.svg" alt="Share on X" class="share-icon" />
+      </a>
+      <a id="facebookShare" href="#" target="_blank" title="Share on Facebook">
+        <img src="img/facebook.svg" alt="Share on Facebook" class="share-icon" />
+      </a>
+      <a id="redditShare" href="#" target="_blank" title="Share on Reddit">
+        <img src="img/reddit.svg" alt="Share on Reddit" class="share-icon" />
+      </a>
+    </div>
   `;
 
   // Always show if valid term
